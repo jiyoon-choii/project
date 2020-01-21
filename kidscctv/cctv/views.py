3c012a0c33469d689c39077151c9b166a0e4aad9
jiyoon-choii <jwjy13@ks.ac.kr>
2020-01-21 19:12:36 +0900

    20200121

--- kidscctv/cctv/views.py
+++ kidscctv/cctv/views.py
@@ -15,9 +15,17 @@ def article_main(request):
         cnt = article.objects.all().count()
         total = (cnt-1)//6+1    
 
+        
+        imgs = ["/static/image/art_thumbnail/['newsis' + str(i) + '.png']"]
+        for i in imgs :
+            
+
+            
+        
 
 
-        return render(request,'article_main2.html',{"list":list1, "pages":range(1,total+1,1)})
+
+        return render(request,'article_main2.html',{"list":list1, "pages":range(1,total+1,1)}, "img":img)
         # return render(request,'article_main.html',{"list":rows, 'img': "/static/image/art_thumbnail/['newsis' + str(i) + '.png']", "cnt":range(60)})
      
 
